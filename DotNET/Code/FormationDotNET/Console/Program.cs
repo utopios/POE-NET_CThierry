29063a307fbb06eddf25ff0b7000b9294731a5bb
uthopios <github@utopios.net>
2021-12-07 17:16:28 +0100

    Ajout Support de cours Langage C#

---
+++ DotNET/Code/FormationDotNET/Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+
+
+            System.Console.WriteLine("Appuyez sur entrer pour quitter");
+            System.Console.Read();
+        }
+    }
+}
