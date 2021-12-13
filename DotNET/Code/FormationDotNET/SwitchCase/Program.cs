f329a95c0f96bde97baca6b037f20efcbdbb0fcc
uthopios <github@utopios.net>
2021-12-13 16:31:21 +0100

    Ajout cours switchCase Part 2

--- DotNET/Code/FormationDotNET/SwitchCase/Program.cs
+++ DotNET/Code/FormationDotNET/SwitchCase/Program.cs
@@ -26,14 +26,15 @@ namespace SwitchCase
             }
             #endregion
 
+
             #region Avec un int en condition d'accès
             Console.Write("Veuillez saisir 1, 5, 12 ou 18 : ");
-            int age = Convert.ToInt32( Console.ReadLine());
+            int age = Convert.ToInt32(Console.ReadLine());
             switch (age)
             {
                 case 1:
                     Console.WriteLine("Vous êtes mineur");
-                    break ;
+                    break;
                 case 5:
                     Console.WriteLine("Vous êtes un enfant");
                     goto case 1;
@@ -48,6 +49,35 @@ namespace SwitchCase
                     break;
             }
             #endregion
+
+
+            #region Switch avec range
+            switch (age)
+            {
+                case int ageTmp when ageTmp < 0:
+                    Console.WriteLine("Heuuu... vous êtes sur...?");
+                    break; 
+                case int ageTmp when ageTmp < 18:
+                    Console.WriteLine("Vous êtes mineur");
+                    break; 
+                case int ageTmp when ageTmp >= 18 && ageTmp<60:
+                    Console.WriteLine("Vous êtes actif");
+                    break;
+                case int ageTmp when ageTmp >= 60 && ageTmp < 123:
+                    Console.WriteLine("Vous êtes retraité");
+                    break;
+                case int ageTmp when ageTmp >= 123:
+                    Console.WriteLine("Record battu! Appelez le guiness Book");
+                    break;
+                default:
+                    Console.WriteLine("Merci de saisir une age !");
+                    break;
+            }
+            #endregion
+
+
+
+            Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
             Console.Read();
         }
     }
