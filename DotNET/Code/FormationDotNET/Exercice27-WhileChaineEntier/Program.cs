5f0d6c1eb7a2cbd232d168ef4b3bfa5842d7a2ee
uthopios <github@utopios.net>
2021-12-16 09:56:39 +0100

    Correction Exercice 28

---
+++ DotNET/Code/FormationDotNET/Exercice27-WhileChaineEntier/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercice27_WhileChaineEntier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Déclaration de variable
+            int nombre;
+            #endregion
+
+            #region Récupération saisie utilisateur et affichage informations
+            Console.WriteLine("--- Les suites chaînées de nombres --- \n");
+            Console.Write("Merci de saisir un nombre : ");
+            nombre = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Les chaînes possible sont : ");
+            #endregion
+
+            #region Instruction While pour rechercher les chaînes et les afficher
+            int i = 1;
+            while (i <= nombre / 2 + 1)
+            {
+                int sommeTmp = i;
+                string chaineTmp = nombre + " = " + i;
+                int j = i + 1;
+                while (j <= nombre / 2 + 1)
+                {
+                    sommeTmp = sommeTmp + j;
+                    chaineTmp += "+" + j;
+                    if (sommeTmp == nombre)
+                    {
+                        Console.WriteLine(chaineTmp);
+                        break;
+                    }
+                    else if (sommeTmp > nombre)
+                        break;
+                    j++;
+                }
+                i++;
+            }
+            #endregion
+
+            
+            Console.WriteLine("\nAppuyez sur Entrer pour fermer le programme...");
+            Console.Read();
+        }
+    }
+}
---
+++ DotNET/Code/FormationDotNET/Exercice28-NbMystere/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exercice28_NbMystere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Déclaration des variables
+            Random aleatoire = new Random();
+            int nbMystere = aleatoire.Next(1, 51);
+            int nbCoups = 0;
+            bool trouve = false;
+            //Console.WriteLine(nbMystere);
+            #endregion
+
+            #region Boucle While : Tant que le nombre mystere n'est pas trouvé
+            Console.WriteLine("--- Trouver le nombre mystere ---\n");
+            while (!trouve)
+            {
+                Console.Write("\tVeuillez saisir un nombre : ");
+                int nbTmp= Convert.ToInt32(Console.ReadLine());
+                nbCoups++;
+                if (nbTmp == nbMystere )
+                {
+                    trouve = true;
+                }
+                else if (nbTmp < nbMystere)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\t\tLe nombre mystere est plus grand");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\t\tLe nombre mystere est plus petit");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            #endregion
+
+            #region Affichage des résultats
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nBravo vous avez trouve le nombre {nbMystere}");
+            Console.WriteLine($"Vous avez trouvé en {nbCoups} coups");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
