fd443e5acaf0132a6b9983fab7c6dd6998d1f590
uthopios <github@utopios.net>
2021-12-21 17:28:56 +0100

    Debut Exercice Chaise

---
+++ DotNET/Code/FormationDotNET/TpClasseChaise/Classes/Chaise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpClasseChaise.Classes
+{
+    internal class Chaise
+    {
+    }
+}
