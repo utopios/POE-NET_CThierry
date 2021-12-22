5c945b710e736c1152d04c024a1b5ecd4600ef9d
uthopios <github@utopios.net>
2021-12-22 09:35:08 +0100

    Ajout correction Tp Classe Chaise

--- DotNET/Code/FormationDotNET/TpClasseChaise/Classes/Chaise.cs
+++ DotNET/Code/FormationDotNET/TpClasseChaise/Classes/Chaise.cs
@@ -8,5 +8,39 @@ namespace TpClasseChaise.Classes
 {
     internal class Chaise
     {
+        // Déclaration des attributs privés (protégés par l'encapsulation)
+        private int nbPieds;
+        private string couleur;
+        private string materiaux;
+
+        public Chaise()
+        {
+
+        }
+
+        public Chaise(int nbPieds, string couleur, string materiaux)
+        {
+            NbPieds=nbPieds;
+            Couleur=couleur;
+            Materiaux=materiaux;
+        }
+
+
+        // Propriétés publiques permettant d'acceder aux attibuts
+        public int NbPieds { get => nbPieds; set => nbPieds=value; }
+        public string Couleur { get => couleur; set => couleur=value; }
+        public string Materiaux { get => materiaux; set => materiaux=value; }
+
+        public override string ToString()
+        {
+            return $"Nb Pieds : {NbPieds} - Matériaux : {Materiaux} - Couleur : {Couleur}";
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("---------------------- Affichage d'un nouvel objet ----------------------\n");
+            Console.WriteLine($"Je suis une chaise avec {NbPieds} pieds en {Materiaux} et de couleur {Couleur}\n");
+            Console.WriteLine("-------------------------------------------------------------------------\n");
+        }
     }
 }
--- DotNET/Code/FormationDotNET/TpClasseChaise/Program.cs
+++ DotNET/Code/FormationDotNET/TpClasseChaise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TpClasseChaise.Classes;
 
 namespace TpClasseChaise
 {
@@ -6,7 +7,32 @@ namespace TpClasseChaise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Chaise 1
+            Chaise chaise1 = new Chaise();
+            chaise1.NbPieds = 4;
+            chaise1.Couleur = "Bleu";
+            chaise1.Materiaux= "Bois";
+
+            chaise1.Afficher();
+            #endregion
+
+            #region Chaise 2
+            Chaise chaise2 = new Chaise(4, "Blanche", "Métal");
+            chaise2.Afficher();
+            #endregion
+
+
+            #region Chaise 3
+            Chaise chaise3 = new Chaise(1, "Transparente", "Pléxiglass");
+            chaise3.Afficher();
+            #endregion
+
+            Console.WriteLine(chaise3);
+
+            Console.WriteLine("Appuyez sur ENTER pour fermer le programme");
+            Console.Read();
+
         }
+
     }
 }
