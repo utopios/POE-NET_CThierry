a5d0a557eef35192260b855f8e3ff2f8aa747246
PetitVulcan <dipersio.a@gmail.com>
2022-01-05 14:51:37 +0100

    Ajout section Enum

---
+++ DotNET/Code/FormationDotNET/LesEnums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LesEnums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saison a = Saison.Hiver;
+            Console.WriteLine($"La velaur numérique de {a} est {(int)a}");
+
+            var b = (Saison)1;
+            Console.WriteLine(b);
+
+            var c = (Saison)0;
+            Console.WriteLine(c);
+
+            Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
+            Console.Read();
+        }
+
+        public enum Saison
+        {
+            Printemps,// 0
+            Eté,// 1
+            Automne, // 2
+            Hiver // 3
+        }
+    }
+}
