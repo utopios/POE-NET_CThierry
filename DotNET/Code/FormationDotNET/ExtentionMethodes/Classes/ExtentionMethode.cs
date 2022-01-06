36c48a4dc9a2c4b22b6d0bbae8db69b662cbea20
PetitVulcan <dipersio.a@gmail.com>
2022-01-06 15:42:42 +0100

    Ajout cours extention de méthode

---
+++ DotNET/Code/FormationDotNET/ExtentionMethodes/Classes/ExtentionMethode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethodes.Classes
+{
+    static class ExtentionMethode
+    {
+        public static void Shuffle<T>(this List<T> liste)
+        {
+            
+            for (int i = 0; i < liste.Count; i++)
+            {
+                Random random = new Random();
+                int index = random.Next(liste.Count);
+                var tmp = liste[i];
+                liste[i] = liste[index];
+                liste[index] = tmp;
+            }
+        }
+    }
+}
---
+++ DotNET/Code/FormationDotNET/ExtentionMethodes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ExtentionMethodes.Classes;
+
+namespace ExtentionMethodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            l.Shuffle();
+
+            foreach (int i in l)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Appuyez sur ENTER pour fermer le programe...");
+            Console.Read();
+        }
+    }
+}
