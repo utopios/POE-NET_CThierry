4228dd444a02f3448bfa26d28d0b1a9f324be202
PetitVulcan <dipersio.a@gmail.com>
2022-01-10 10:00:39 +0100

    Cours Exceptions

---
+++ DotNET/Code/FormationDotNET/LesExceptions/Classes/ExceptionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesExceptions.Classes
+{
+    internal class ExceptionTest
+    {
+        public static double SafeDivision(double nombre, double diviseur)
+        {
+            if (diviseur == 0)
+            {
+                throw new DivideByZeroException("Attention, vous tentez de diviser par 0, c'est impossible!");
+            }
+            return nombre / diviseur;
+        }
+    }
+}
---
+++ DotNET/Code/FormationDotNET/LesExceptions/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using LesExceptions.Classes;
+
+namespace LesExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------- Division -----------------");
+            Console.Write("Veuillez saisir un Chiffre / Nombre à diviser : ");
+            double nb1=-1;
+            bool valid=false;
+            //while (!double.TryParse(Console.ReadLine(),out nb1))
+            //{
+            //    Console.Write("Erreur de saisie, Veuillez saisir un chiffre / nombre : ");
+            //}
+            while (!valid)
+            {
+                try
+                {
+                    nb1 = Convert.ToDouble(Console.ReadLine());
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.Write($"{e.Message} Veuillez saisir un Chiffre / nombre : ");
+                }
+            }
+            
+
+            Console.Write("Veuillez saisir un Chiffre / Nombre diviseur : ");
+            double nb2=-1;
+            valid = false;
+            //while (!double.TryParse(Console.ReadLine(), out nb2))
+            //{
+            //    Console.Write("Erreur de saisie, Veuillez saisir un chiffre / nombre pour le diviseur : ");
+            //}
+
+            while (!valid)
+            {
+                try
+                {
+                    nb2 = Convert.ToDouble(Console.ReadLine());
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.Write($"{e.Message} Veuillez saisir un Chiffre / nombre : ");
+                }
+            }
+
+            double result;
+
+            try
+            {
+                result = ExceptionTest.SafeDivision(nb1, nb2);
+                Console.WriteLine("{0} divisé par {1} = {2}",nb1,nb2,result);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Cette instruction est appliquée quelque soit l'issue du Try");
+            }
+
+
+            Console.WriteLine("Appuyez sur ENTER pour fermer le programme...");
+            Console.Read();
+        }
+    }
+}
