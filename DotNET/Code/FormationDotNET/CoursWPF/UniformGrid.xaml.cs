7afe0cf0cf6d6a13d47fec1172e4fe88ea4f2987
PetitVulcan <dipersio.a@gmail.com>
2022-01-18 09:07:36 +0100

    Ajout UniformGrid WPF

---
+++ DotNET/Code/FormationDotNET/CoursWPF/UniformGrid.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour UniformGrid.xaml
+    /// </summary>
+    public partial class UniformGrid : Window
+    {
+        public UniformGrid()
+        {
+            InitializeComponent();
+        }
+
+        private void Valider_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
