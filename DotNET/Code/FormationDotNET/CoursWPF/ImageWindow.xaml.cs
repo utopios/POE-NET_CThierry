3ceafe6f3d4ad17303de189e4562e6c25431d925
PetitVulcan <dipersio.a@gmail.com>
2022-01-18 09:55:45 +0100

    Ajout image WPF

---
+++ DotNET/Code/FormationDotNET/CoursWPF/ImageWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour ImageWindow.xaml
+    /// </summary>
+    public partial class ImageWindow : Window
+    {
+        public ImageWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Changer_Click(object sender, RoutedEventArgs e)
+        {
+            int index = -1;
+            var images = ImagesDockPanel.Children.OfType<Image>();
+            Image image = images.FirstOrDefault(img => img.Visibility == Visibility.Visible);
+            image.Visibility = Visibility.Collapsed;
+            Image[] imgTmp = images.ToArray();
+            index = Array.IndexOf(imgTmp, image);
+            string nameTmp;
+            if (index <2)
+            {
+                index++;
+                nameTmp = "Img" + index;
+            }
+            else
+            {
+                nameTmp = "Img0";
+            }
+            Image tmp = images.FirstOrDefault(img => img.Name == nameTmp);
+            tmp.Visibility = Visibility.Visible;
+        }
+    }
+}
