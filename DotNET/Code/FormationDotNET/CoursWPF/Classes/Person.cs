62bb0c3729ab4bfc917f9866feedc47ad2d2f63f
PetitVulcan <dipersio.a@gmail.com>
2022-01-18 10:16:18 +0100

    Ajout Affichage ListView

---
+++ DotNET/Code/FormationDotNET/CoursWPF/Classes/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursWPF.Classes
+{
+    internal class Person
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+
+        public Person(string nom, string prenom, int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
---
+++ DotNET/Code/FormationDotNET/CoursWPF/ListViewWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using CoursWPF.Classes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour ListViewWindow.xaml
+    /// </summary>
+    public partial class ListViewWindow : Window
+    {
+        internal ObservableCollection<Person> personList;
+        public ListViewWindow()
+        {
+            InitializeComponent();
+            personList = new ObservableCollection<Person>();
+            FillListPerson();
+            RefreshLisBoxView();
+        }
+
+        private void FillListPerson()
+        {
+            Person p1 = new Person("Di Persio", "Anthony", 25);
+            Person p2 = new Person("Toto", "Titi", 15);
+            Person p3 = new Person("Foo", "Bar", 35);
+            Person p4 = new Person("Doe", "John", 45);
+            personList.Add(p1);
+            personList.Add(p2);
+            personList.Add(p3);
+            personList.Add(p4);
+        }
+
+        private void RefreshLisBoxView()
+        {
+            ListBoxView.ItemsSource = personList;
+        }
+
+    }
+}
