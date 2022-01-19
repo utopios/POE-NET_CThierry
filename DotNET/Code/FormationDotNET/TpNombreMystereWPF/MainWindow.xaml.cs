127f73216ea14dbf83c3d2555314f38d39807899
PetitVulcan <dipersio.a@gmail.com>
2022-01-19 11:53:16 +0100

    Correction TpNbMystereWPF Part 2

--- DotNET/Code/FormationDotNET/TpNombreMystereWPF/MainWindow.xaml.cs
+++ DotNET/Code/FormationDotNET/TpNombreMystereWPF/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace TpNombreMystereWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Attributs
+        static Random aleatoire = new Random();
+        int nbMystere;
+        int nbCoups;        
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            int nbMystere = aleatoire.Next(1, 51);
+            int nbCoups = 0;
+            TextBoxResult1.Text = "test";
+            TextBoxResult2.Text = nbMystere.ToString();
+        }
+
+        private void Valider_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlusGrand()
+        {
+            
+        }
+
+        private void PlusPetit()
+        {
+
+        }
+        private void Win()
+        {
+
+        }
+        void Comparer(int nbUser)
+        {
+            if (nbUser == nbMystere)
+                Win();
+            else if (nbUser < nbMystere)
+                PlusGrand();
+            else
+                PlusPetit();
         }
     }
 }
