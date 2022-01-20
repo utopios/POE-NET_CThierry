edb690b49bede70e64db00b803b285e0bf80935f
PetitVulcan <dipersio.a@gmail.com>
2022-01-20 11:55:52 +0100

    Ajout Cours pattern MVVM

---
+++ DotNET/Code/FormationDotNET/CoursMVVM/Models/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace CoursMVVM.Models
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string gender;
+        private bool isM;
+        private bool isF;
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                //RaisePropertyChanged("FirstName");
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                //RaisePropertyChanged("lastName");
+            }
+        }
+        //public string Gender
+        //{
+        //    get => gender;
+        //    set
+        //    {
+        //        gender = value;
+        //        //RaisePropertyChanged("gender");
+        //    }
+        //}
+        public bool IsM
+        {
+            get => isM;
+            set
+            {
+                isM = value;
+                //RaisePropertyChanged("isM");
+            }
+        }
+        public bool IsF
+        {
+            get => isF;
+            set
+            {
+                isF = value;
+                //RaisePropertyChanged("isF");
+            }
+        }
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //public void RaisePropertyChanged(string propertyName)
+        //{
+        //    if (PropertyChanged != null)
+        //    {
+        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
+    }
+}
+
---
+++ DotNET/Code/FormationDotNET/CoursMVVM/ViewModels/PersonViewModel.cs
@@ -0,0 +1,82 @@
+﻿using CoursMVVM.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CoursMVVM.ViewModels
+{
+    class PersonViewModel : ViewModelBase
+    {
+        private Person person;
+
+        public string FirstName
+        {
+            get => person.FirstName;
+            set
+            {
+                person.FirstName = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string LastName
+        {
+            get => person.LastName;
+            set
+            {
+                person.LastName = value;
+                RaisePropertyChanged();
+            }
+        }
+        
+        public bool IsM
+        {
+            get => person.IsM;
+            set
+            {
+                person.IsM = value;
+                RaisePropertyChanged("Gender");
+            }
+        }
+        public bool IsF
+        {
+            get => person.IsF;
+            set
+            {
+                person.IsF = value;
+                RaisePropertyChanged("Gender");
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                if (IsM)
+                    return "Mr";
+                else if (IsF)
+                    return "Mme";
+                else
+                    return null;
+            }
+        }
+
+        public ICommand ValidCommand { get; set; }
+
+        public PersonViewModel()
+        {
+            person = new Person();
+            ValidCommand = new RelayCommand(ActionValidCommand);
+        }
+
+        private void ActionValidCommand()
+        {
+            MessageBox.Show(Gender + " " + LastName + " " + FirstName); 
+        }
+    }
+}
