44606240b26579873cbeadf5d2f115c1f5c25244
uthopios <github@utopios.net>
2022-01-26 12:30:55 +0100

    Ajout projet cours ASP.NET CORE

---
+++ DotNET/Code/FormationDotNET/CoursAspNET/Controllers/PersonController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoursAspNET.Models;
+
+namespace CoursAspNET.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: PersonController
+        public IActionResult Index()
+        {
+            List<Person> liste = Person.GetPersons();
+            return View(liste);
+        }
+
+        // GET: PersonController/Details/6
+        public IActionResult Details(int id)
+        {
+            // Person person = Person.GetPerson(id);
+            Person p = new Person() { LastName="Di Persio", FirstName="Anthony",Id=id};
+            return View(p);
+        }
+
+        // GET: PersonController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DotNET/Code/FormationDotNET/CoursAspNET/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace CoursAspNET.Models
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        int id;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Id { get => id; set => id = value; }
+
+        public static List<Person> GetPersons()
+        {
+            return new List<Person>() {
+                new Person()  {Id = 1 ,FirstName="Toto", LastName="Tata"},
+                new Person()  {Id = 2 ,FirstName="Minet", LastName="Titi"},
+            };
+        }
+
+        public static Person GetPerson(int id)
+        {
+            Person p = new Person();
+            return p;
+        }
+    }
+}
